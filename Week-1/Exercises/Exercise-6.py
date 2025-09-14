17042a3207a95f3254a275e255aecaa9d80aa78e
Bihan Wang <bihanwang@BihandeMacBook-Pro.local>
2025-09-14 11:46:01 +0200

    Added answers to Exercise 6

--- Week-1/Exercises/Exercise-6.py
+++ Week-1/Exercises/Exercise-6.py
@@ -5,11 +5,17 @@ Hint: Write an is_factor helper function.
 """
 
 def is_factor(d, n):
-    """True iff (if and only if) d is a divisor of n."""
-    pass
+    if n % d == 0:
+        return True
+
 
 def is_prime(n):
-    pass
+    if n < 2 :
+        return False
+    for d in range(2, n):
+        if is_factor(d,n) == True:
+            return False
+    return True
 
-list_of_primes = []
+list_of_primes = [n for n in range(1,10001) if is_prime(n)]
 print(list_of_primes)
